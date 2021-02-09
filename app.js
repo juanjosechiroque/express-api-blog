8cea40c1949176632b28cfa4c58b80bc20dbf838
juanjosechiroque <juanjosechiroque@gmail.com>
2021-02-08 23:39:59 -0500

    add security settings

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 require('./db');
 
 const indexRouter = require('./routes');
-const { cors, errorGeneralHandler } = require('./middleware');
+const { cors, errorGeneralHandler, security } = require('./middleware');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ limit: "1kb" }));
 app.use(cors);
+app.use(security);
 
 app.use('/api', indexRouter);
 
--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,8 @@
+module.exports.security = function(req, res, next) {
+  res.removeHeader('X-Powered-By');  
+  next();
+}
+
 module.exports.cors = function (req, res, next) {  
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
